aad9563f277a65bd1e60586075c299bb0c228459
Paúl M.a <paulmolanoo@gmail.com>
2021-12-18 11:07:47 +0100

    auth

--- src/app.js
+++ src/app.js
@@ -1,32 +1,34 @@
-import express from 'express'
-import morgan from 'morgan'
-import pkg from '../package.json'
+import express from "express";
+import morgan from "morgan";
+import pkg from "../package.json";
 
-import productsRoutes from './routes/products.routes'
-import authRoutes from './routes/auth.routes'
+import { createRoles } from "./libs/initialSetup";
 
-const app = express()
+import productsRoutes from "./routes/products.routes";
+import authRoutes from "./routes/auth.routes";
+
+const app = express();
+createRoles();
 
 //Metodo para guardar una variable en express
-app.set('pkg', pkg)
+app.set("pkg", pkg);
 
 //Para que lea los archivos en formato json
-app.use(express.json())
+app.use(express.json());
 
 //Middleware para ver en consola cada vez que hacen una peticion a nuestro servidor
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 
-app.get('/', (req, res) =>{
-    res.json({
-        name: app.get('pkg').name,
-        author: app.get('pkg').author,
-        description: app.get('pkg').description,
-        version: app.get('pkg').version
-    })
-})
+app.get("/", (req, res) => {
+  res.json({
+    name: app.get("pkg").name,
+    author: app.get("pkg").author,
+    description: app.get("pkg").description,
+    version: app.get("pkg").version,
+  });
+});
 
-app.use('/api/products', productsRoutes)
-app.use('/api/auth', authRoutes)
+app.use("/api/products", productsRoutes);
+app.use("/api/auth", authRoutes);
 
 export default app;
-
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,24 +1,34 @@
-import User from '../models/User'
+import User from "../models/User";
+import jwt from "jsonwebtoken";
+import config from "../config";
+import Role from "../models/Role";
 
-export const signUp = async (req, res) =>{
-    const {username, email, password, roles} = req.body;
+export const signUp = async (req, res) => {
+  const { username, email, password, roles } = req.body;
 
-    const newUser = new User({
-        username: username,
-        email: email,
-        password: await User.encryptPassword(password),
-    })
+  const newUser = new User({
+    username: username,
+    email: email,
+    password: await User.encryptPassword(password),
+  });
 
-    console.log(newUser);
-    res.json('signup')
-}
+  if (roles) {
+    const foundRoles = await Role.find({ name: { $in: roles } });
+    newUser.roles = foundRoles.map((role) => role._id);
+  } else {
+    const role = await Role.findOne({ name: "user" });
+    newUser.roles = [role._id];
+  }
 
-export const signin = async (req, res) =>{
-    const {username, email, password, roles} = req.body;
+  const savedUser = await newUser.save();
 
-    console.log(req.body);
+  const token = jwt.sign({ id: savedUser._id }, config.SECRET, {
+    expiresIn: 86400, //24hours
+  });
 
+  res.status(200).json({ token });
+};
 
-    res.json('siginp')
-    
-}
+export const signin = async (req, res) => {
+  const { username, email } = req.body;
+};

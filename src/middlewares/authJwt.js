8f1aa6e46ffc35c7f5085d83235441e338266741
Paúl M.a <paulmolanoo@gmail.com>
2021-12-18 12:24:16 +0100

    middlewares

---
+++ src/middlewares/authJwt.js
@@ -0,0 +1,20 @@
+//autorizacion
+import jwt from "jsonwebtoken";
+import config from "../config";
+import User from "../models/User";
+
+export const verifyToken = async (req, res, next) => {
+  const token = req.headers["x-access-token"];
+
+  if (!token) return res.status(403).json({ message: "No token provided" });
+
+  const decoded = jwt.verify(token, config.SECRET);
+  req.userId = decoded.id;
+
+  const user = await User.findById(req.userId, { password: 0 }).populate(
+    "roles"
+  );
+  if (!user) return res.status(404).json({ message: "no user found" });
+  console.log(user);
+  next();
+};
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,12 +1,13 @@
-import {Router} from 'express'
-const router = Router()
+import { Router } from "express";
+const router = Router();
 
 import * as productCtrl from "../controllers/products.controller";
+import { verifyToken } from "../middlewares";
 
-router.post('/', productCtrl.createProduct)
-router.get('/', productCtrl.getProducts)
-router.get('/:productId', productCtrl.getProductById)
-router.put('/:productId', productCtrl.updateProductById)
-router.delete('/:productId', productCtrl.deleteProductById)
+router.post("/", verifyToken, productCtrl.createProduct);
+router.get("/", productCtrl.getProducts);
+router.get("/:productId", productCtrl.getProductById);
+router.put("/:productId", verifyToken, productCtrl.updateProductById);
+router.delete("/:productId", verifyToken, productCtrl.deleteProductById);
 
-export default router
+export default router;

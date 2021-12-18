cb5162655d1d5517fb3cbd791bea82dca204a237
Paúl M.a <paulmolanoo@gmail.com>
2021-12-18 11:20:34 +0100

    signin

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -30,5 +30,23 @@ export const signUp = async (req, res) => {
 };
 
 export const signin = async (req, res) => {
-  const { username, email } = req.body;
+  const userFound = await User.findOne({ email: req.body.email }).populate(
+    "roles"
+  );
+
+  if (!userFound) return res.status(400).json({ message: "User not found" });
+
+  const matchPassword = await User.comparePassword(
+    req.body.password,
+    userFound.password
+  );
+
+  if (!matchPassword)
+    return res.status(401).json({ token: null, message: "Invalid password" });
+
+  const token = jwt.sign({ id: userFound._id }, config.SECRET, {
+    expiresIn: 86400,
+  });
+
+  res.json({ token });
 };
